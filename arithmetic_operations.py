48ef585246100031b28de3f30d789be68a5968ab
Ayush1057 <93645403+Ayush1057@users.noreply.github.com>
2022-09-07 18:58:45 +0530

    Add files via upload

---
+++ arithmetic_operations.py
@@ -0,0 +1,44 @@
+#Discription: Arithmetic operations~~
+
+#Addition 
+print("Addtion:")
+
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+
+c=a+b
+
+print("The Addtion of a and b is ",c)
+
+
+#Subtraction
+print("Subtraction")
+
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+
+c=a+b
+
+print("The Subtraction of a and b is ",c)
+
+
+#Division
+print("Division")
+
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+
+c=a/b
+
+print("The Division of a and b is ",c)
+
+
+#Multiplication
+print("Multiplication")
+
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+
+c=a*b
+
+print("The Multiplication of a and b is ",c)
