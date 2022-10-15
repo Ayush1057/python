99777d37071ddf903b77e4b56fede4810fe604ed
Ayush1057 <93645403+Ayush1057@users.noreply.github.com>
2022-10-15 17:37:03 +0530

    Create triangle.py

---
+++ triangle.py
@@ -0,0 +1,8 @@
+a = int(input("Enter first angle "))
+b = int(input("Enter second angle "))
+c = int(input("Enter third angle "))
+
+if a+b+c==180:
+    print("Triangle can be formed")
+else:
+    print("Triangle cannot be formed")
